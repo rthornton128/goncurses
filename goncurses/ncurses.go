971243cf7c163bf99a7598cb6908be1b493f69f8
Rob Thornton <robthornton128@gmail.com>
2012-01-22 19:52:10 -0800

    Adjusted line length for license in ncurses.go Added some missing descriptions for key constants Removed some dead code Added 12 new functions Fixed a couple minor formatting issues with gofmt

--- goncurses/ncurses.go
+++ goncurses/ncurses.go
@@ -10,13 +10,13 @@
 //   * Redistributions of source code must retain the above copyright notice, 
 //     this list of conditions and the following disclaimer.
 //
-//   * Redistributions in binary form must reproduce the above copyright notice, 
-//     this list of conditions and the following disclaimer in the documentation 
-//     and/or other materials provided with the distribution.
+//   * Redistributions in binary form must reproduce the above copyright 
+//     notice, this list of conditions and the following disclaimer in the 
+//     documentation and/or other materials provided with the distribution.
 //  
 //   * Neither the name of the copyright holder nor the names of its 
-//     contributors may be used to endorse or promote products derived from this 
-//     software without specific prior written permission.
+//     contributors may be used to endorse or promote products derived from 
+//     this software without specific prior written permission.
 //      
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
@@ -148,38 +148,38 @@ const (
 )
 
 const (
-	KEY_TAB       = 9           // tab
-	KEY_RETURN    = 10          // enter key vs. KEY_ENTER
-	KEY_DOWN      = C.KEY_DOWN  // down arrow key
-	KEY_UP        = C.KEY_UP    // up arrow key
-	KEY_LEFT      = C.KEY_LEFT  // left arrow key
-	KEY_RIGHT     = C.KEY_RIGHT // right arrow key
-	KEY_HOME      = C.KEY_HOME
-	KEY_BACKSPACE = C.KEY_BACKSPACE
-	KEY_F1        = C.KEY_F0 + 1
-	KEY_F2        = C.KEY_F0 + 2
-	KEY_F3        = C.KEY_F0 + 3
-	KEY_F4        = C.KEY_F0 + 4
-	KEY_F5        = C.KEY_F0 + 5
-	KEY_F6        = C.KEY_F0 + 6
-	KEY_F7        = C.KEY_F0 + 7
-	KEY_F8        = C.KEY_F0 + 8
-	KEY_F9        = C.KEY_F0 + 9
-	KEY_F10       = C.KEY_F0 + 10
-	KEY_F11       = C.KEY_F0 + 11
-	KEY_F12       = C.KEY_F0 + 12
-	KEY_DL        = C.KEY_DL    // delete-line key
-	KEY_IL        = C.KEY_IL    // insert-line key
-	KEY_DC        = C.KEY_DC    // delete-character key
-	KEY_IC        = C.KEY_IC    // insert-character key
-	KEY_EIC       = C.KEY_EIC   // sent by rmir or smir in insert mode
-	KEY_CLEAR     = C.KEY_CLEAR // clear-screen or erase key 
-	KEY_EOS       = C.KEY_EOS   // clear-to-end-of-screen key 
-	KEY_EOL       = C.KEY_EOL   // clear-to-end-of-line key 
-	KEY_SF        = C.KEY_SF    // scroll-forward key 
-	KEY_SR        = C.KEY_SR    // scroll-backward key 
-	KEY_PAGEDOWN  = C.KEY_NPAGE
-	KEY_PAGEUP    = C.KEY_PPAGE
+	KEY_TAB       = 9               // tab
+	KEY_RETURN    = 10              // enter key vs. KEY_ENTER
+	KEY_DOWN      = C.KEY_DOWN      // down arrow key
+	KEY_UP        = C.KEY_UP        // up arrow key
+	KEY_LEFT      = C.KEY_LEFT      // left arrow key
+	KEY_RIGHT     = C.KEY_RIGHT     // right arrow key
+	KEY_HOME      = C.KEY_HOME      // home key
+	KEY_BACKSPACE = C.KEY_BACKSPACE // backpace
+	KEY_F1        = C.KEY_F0 + 1    // F1 key
+	KEY_F2        = C.KEY_F0 + 2    // F2 key
+	KEY_F3        = C.KEY_F0 + 3    // F3 key
+	KEY_F4        = C.KEY_F0 + 4    // F4 key
+	KEY_F5        = C.KEY_F0 + 5    // F5 key
+	KEY_F6        = C.KEY_F0 + 6    // F6 key
+	KEY_F7        = C.KEY_F0 + 7    // F7 key
+	KEY_F8        = C.KEY_F0 + 8    // F8 key
+	KEY_F9        = C.KEY_F0 + 9    // F9 key
+	KEY_F10       = C.KEY_F0 + 10   // F10 key 
+	KEY_F11       = C.KEY_F0 + 11   // F11 key
+	KEY_F12       = C.KEY_F0 + 12   // F12 key
+	KEY_DL        = C.KEY_DL        // delete-line key
+	KEY_IL        = C.KEY_IL        // insert-line key
+	KEY_DC        = C.KEY_DC        // delete-character key
+	KEY_IC        = C.KEY_IC        // insert-character key
+	KEY_EIC       = C.KEY_EIC       // sent by rmir or smir in insert mode
+	KEY_CLEAR     = C.KEY_CLEAR     // clear-screen or erase key 
+	KEY_EOS       = C.KEY_EOS       // clear-to-end-of-screen key 
+	KEY_EOL       = C.KEY_EOL       // clear-to-end-of-line key 
+	KEY_SF        = C.KEY_SF        // scroll-forward key 
+	KEY_SR        = C.KEY_SR        // scroll-backward key 
+	KEY_PAGEDOWN  = C.KEY_NPAGE     // page-down key (next-page)
+	KEY_PAGEUP    = C.KEY_PPAGE     // page-up key (prev-page)
 	KEY_STAB      = C.KEY_STAB      // set-tab key 
 	KEY_CTAB      = C.KEY_CTAB      // clear-tab key 
 	KEY_CATAB     = C.KEY_CATAB     // clear-all-tabs key 
@@ -281,8 +281,6 @@ var keyList = map[C.int]string{
 	C.KEY_PPAGE:     "page up",
 }
 
-//type MMask C.mmask_t
-
 // Mouse button events
 const (
 	M_ALL            = C.ALL_MOUSE_EVENTS
@@ -311,39 +309,11 @@ const (
 	M_SHIFT          = C.BUTTON_SHIFT          // shift-click
 	M_POSITION       = C.REPORT_MOUSE_POSITION // mouse moved
 )
-/*
-var mouseEvents = map[string]MMask{
-	"button1-pressed":        C.BUTTON1_PRESSED,
-	"button1-released":       C.BUTTON1_RELEASED,
-	"button1-clicked":        C.BUTTON1_CLICKED,
-	"button1-double-clicked": C.BUTTON1_DOUBLE_CLICKED,
-	"button1-triple-clicked": C.BUTTON1_TRIPLE_CLICKED,
-	"button2-pressed":        C.BUTTON2_PRESSED,
-	"button2-released":       C.BUTTON2_RELEASED,
-	"button2-clicked":        C.BUTTON2_CLICKED,
-	"button2-double-clicked": C.BUTTON2_DOUBLE_CLICKED,
-	"button2-triple-clicked": C.BUTTON2_TRIPLE_CLICKED,
-	"button3-pressed":        C.BUTTON3_PRESSED,
-	"button3-released":       C.BUTTON3_RELEASED,
-	"button3-clicked":        C.BUTTON3_CLICKED,
-	"button3-double-clicked": C.BUTTON3_DOUBLE_CLICKED,
-	"button3-triple-clicked": C.BUTTON3_TRIPLE_CLICKED,
-	"button4-pressed":        C.BUTTON4_PRESSED,
-	"button4-released":       C.BUTTON4_RELEASED,
-	"button4-clicked":        C.BUTTON4_CLICKED,
-	"button4-double-clicked": C.BUTTON4_DOUBLE_CLICKED,
-	"button4-triple-clicked": C.BUTTON4_TRIPLE_CLICKED,
-	//    "button5-pressed": C.BUTTON5_PRESSED,
-	//    "button5-released": C.BUTTON5_RELEASED,
-	//    "button5-clicked": C.BUTTON5_CLICKED,
-	//    "button5-double-clicked": C.BUTTON5_DOUBLE_CLICKED,
-	//    "button5-triple-clicked": C.BUTTON5_TRIPLE_CLICKED,
-	"shift":    C.BUTTON_SHIFT,
-	"ctrl":     C.BUTTON_CTRL,
-	"alt":      C.BUTTON_ALT,
-	"all":      C.ALL_MOUSE_EVENTS,
-	"position": C.REPORT_MOUSE_POSITION,
-}*/
+
+// BaudRate returns the speed of the terminal in bits per second
+func BaudRate() int {
+	return int(C.baudrate())
+}
 
 // Beep requests the terminal make an audible bell or, if not available,
 // flashes the screen. Note that screen flashing doesn't work on all
@@ -362,10 +332,28 @@ func CBreak(on bool) {
 	C.nocbreak()
 }
 
+// Test whether colour values can be changed
+func CanChangeColor() bool {
+	return bool(C.bool(C.can_change_color()))
+}
+
+// Get RGB values for specified colour
+func ColorContent(col int) (int, int, int) {
+	var r, g, b C.short
+	C.color_content(C.short(col), (*C.short)(&r), (*C.short)(&g),
+		(*C.short)(&b))
+	return int(r), int(g), int(b)
+}
+
 // Return the value of a color pair which can be passed to functions which
 // accept attributes like AddChar or AttrOn/Off.
 func ColorPair(pair int) int {
 	return int(C.COLOR_PAIR(C.int(pair)))
+}
+
+// CursesVersion returns the version of the ncurses library currently linked to
+func CursesVersion() string {
+	return C.GoString(C.curses_version())
 }
 
 // Set the cursor visibility. Options are: 0 (invisible/hidden), 1 (normal)
@@ -428,10 +416,24 @@ func HalfDelay(delay int) os.Error {
 	return nil
 }
 
+// HasColors returns true if terminal can display colors
+func HasColors() bool {
+	return bool(C.has_colors())
+}
+
+// HasKey returns true if terminal recognized the given character
+func HasKey(ch int) bool {
+	if C.has_key(C.int(ch)) == 1 {
+		return true
+	}
+	return false
+}
+
 // InitColor is used to set 'color' to the specified RGB values. Values may
 // be between 0 and 1000.
 func InitColor(col int, r, g, b int) os.Error {
-	if C.init_color(C.short(col), C.short(r), C.short(g), C.short(b)) == C.ERR {
+	if C.init_color(C.short(col), C.short(r), C.short(g),
+		C.short(b)) == C.ERR {
 		return os.NewError("Failed to set new color definition")
 	}
 	return nil
@@ -457,6 +459,17 @@ func Init() (stdscr *Window, err os.Error) {
 		err = os.NewError("An error occurred initializing ncurses")
 	}
 	return
+}
+
+// IsEnd returns true if End() has been called, otherwise false
+func IsEnd() bool {
+	return bool(C.isendwin())
+}
+
+// IsTermResized returns true if ResizeTerm would modify any current Windows 
+// if called with the given parameters
+func IsTermResized(nlines, ncols int) bool {
+	return bool(C.is_term_resized(C.int(nlines), C.int(ncols)))
 }
 
 // Returns a string representing the value of input returned by Getch
@@ -514,6 +527,15 @@ func Raw(on bool) {
 		return
 	}
 	C.noraw()
+}
+
+// ResizeTerm will attempt to resize the terminal. This only has an effect if
+// the terminal is in an XWindows (GUI) environment.
+func ResizeTerm(nlines, ncols int) os.Error {
+	if C.resizeterm(C.int(nlines), C.int(ncols)) == C.ERR {
+		return os.NewError("Failed to resize terminal")
+	}
+	return nil
 }
 
 // Enables colors to be displayed. Will return an error if terminal is not
@@ -718,7 +740,7 @@ overlay bool) os.Error {
 // DelChar
 func (w *Window) DelChar(coord ...int) os.Error {
 	if len(coord) > 2 {
-		return os.NewError(fmt.Sprintf("Invalid number of arguments, " +
+		return os.NewError(fmt.Sprintf("Invalid number of arguments, "+
 			"expected 2, got %d", len(coord)))
 	}
 	var err C.int
@@ -818,6 +840,16 @@ func (w *Window) HLine(y, x, ch, wid int) {
 	return
 }
 
+// IsCleared returns the value set in ClearOk
+func (w *Window) IsCleared() bool {
+	return bool(C.is_cleared((*C.WINDOW)(w)))
+}
+
+// IsKeypad returns the value set in Keypad
+func (w *Window) IsKeypad() bool {
+	return bool(C.is_keypad((*C.WINDOW)(w)))
+}
+
 // Keypad turns on/off the keypad characters, including those like the F1-F12 
 // keys and the arrow keys
 func (w *Window) Keypad(keypad bool) os.Error {
@@ -866,6 +898,10 @@ func (w *Window) Overwrite(src *Window) os.Error {
 		return os.NewError("Failed to overwrite window")
 	}
 	return nil
+}
+
+func (w *Window) Parent() *Window {
+	return (*Window)(C.wgetparent((*C.WINDOW)(w)))
 }
 
 // Print a string to the given window. The first two arguments may be
