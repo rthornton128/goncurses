5e199cfc0c943756848abffa5c38b2c19bf83125
Rob Thornton <rthornton128@gmail.com>
2012-07-21 15:31:38 -0700

    Update hello example to conform to go gettable import path

--- examples/hello/hello.go
+++ examples/hello/hello.go
@@ -1,15 +1,14 @@
-/* This simnple example mirrors the "hello world" TLDP ncurses howto */
+/* This simple example mirrors the "hello world" TLDP ncurses howto */
 
 package main
 
-/* Note that is not considered idiomatic Go to import curses this way */
-import . "goncurses.googlecode.com/hg/goncurses"
+import "code.google.com/p/goncurses"
 
 func main() {
-    stdscr, _ := Init()
-    defer End()
-    
-    stdscr.Print("Hello, World!!!")
-    stdscr.Refresh()
-    stdscr.GetChar()
+	stdscr, _ := goncurses.Init()
+	defer goncurses.End()
+
+	stdscr.Print("Hello, World!!!")
+	stdscr.Refresh()
+	stdscr.GetChar()
 }
