008288d3a74e84831fc5fedefa372f80cf6c0d15
Rob Thornton <rthornton128@gmail.com>
2012-07-21 20:40:01 -0700

    Make menu only build on linux, unix and BSD Move menu back to root

--- examples/menu/menu.go
+++ examples/menu/menu.go
@@ -3,56 +3,54 @@
 
 package main
 
-import . "goncurses.googlecode.com/hg/goncurses"
-import "os"
+import "code.google.com/p/goncurses"
 
 const (
-    HEIGHT = 10
-    WIDTH = 30
+	HEIGHT = 10
+	WIDTH  = 30
 )
 
 func main() {
-    stdscr, _ := Init();
-    defer End()
-    
-    Raw(true)
-    Echo(false)
-    Cursor(0)
-    stdscr.Clear()
-    stdscr.Keypad(true)
-    
-    menu_items := []string{"Choice 1", "Choice 2", "Choice 3", "Choice 4", 
-        "Exit"}
-    items := make([]*MenuItem, len(menu_items))
-    var err os.Error
-    for i, val := range menu_items {
-        items[i], err = NewItem(val, "")
-        defer items[i].Free()
-    }
-    
-    menu, err := NewMenu(items)
-    defer menu.Free()
-    menu.Post()
-    
-    if err != nil {
-        stdscr.Print(err.String())
-        return
-    }
+	stdscr, _ := goncurses.Init()
+	defer goncurses.End()
 
-    stdscr.Print(20, 0, "'q' to exit")
-    stdscr.Refresh()
-    
-    for {
-        Update()
-        ch := stdscr.GetChar()
-        
-        switch (Key(ch)) {
-        case "q":
-            return
-        case "down":
-            menu.Driver(REQ_DOWN)
-        case "up":
-            menu.Driver(REQ_UP)
-        }
-    }
+	goncurses.Raw(true)
+	goncurses.Echo(false)
+	goncurses.Cursor(0)
+	stdscr.Clear()
+	stdscr.Keypad(true)
+
+	menu_items := []string{"Choice 1", "Choice 2", "Choice 3", "Choice 4",
+		"Exit"}
+	items := make([]*goncurses.MenuItem, len(menu_items))
+	for i, val := range menu_items {
+		items[i], _ = goncurses.NewItem(val, "")
+		defer items[i].Free()
+	}
+
+	menu, err := goncurses.NewMenu(items)
+	if err != nil {
+		stdscr.Print(err)
+		return
+	}
+	defer menu.Free()
+
+	menu.Post()
+
+	stdscr.Print(20, 0, "'q' to exit")
+	stdscr.Refresh()
+
+	for {
+		goncurses.Update()
+		ch := stdscr.GetChar()
+
+		switch goncurses.KeyString(ch) {
+		case "q":
+			return
+		case "down":
+			menu.Driver(goncurses.REQ_DOWN)
+		case "up":
+			menu.Driver(goncurses.REQ_UP)
+		}
+	}
 }
--- menu_posix.go
+++ menu_posix.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build linux bsd unix
+
 /* ncurses menu extension */
 package goncurses
 
@@ -16,7 +18,7 @@ ITEM* menu_item_at(ITEM** ilist, int i) {
 import "C"
 
 import (
-	//	"os"
+	"errors"
 	"syscall"
 	"unsafe"
 )
@@ -57,7 +59,7 @@ const O_SELECTABLE = C.O_SELECTABLE
 
 // DriverActions is a convenience mapping for common responses
 // to keyboard input
-var DriverActions = map[int]int{
+var DriverActions = map[Key]int{
 	KEY_DOWN:     C.REQ_DOWN_ITEM,
 	KEY_HOME:     C.REQ_FIRST_ITEM,
 	KEY_END:      C.REQ_LAST_ITEM,
@@ -66,6 +68,37 @@ var DriverActions = map[int]int{
 	KEY_PAGEUP:   C.REQ_SCR_UPAGE,
 	KEY_RIGHT:    C.REQ_RIGHT_ITEM,
 	KEY_UP:       C.REQ_UP_ITEM,
+}
+
+var errList = map[C.int]string{
+	C.E_SYSTEM_ERROR:    "System error occurred",
+	C.E_BAD_ARGUMENT:    "Incorrect or out-of-range argument",
+	C.E_POSTED:          "Form has already been posted",
+	C.E_CONNECTED:       "Field is already connected to a form",
+	C.E_BAD_STATE:       "Bad state",
+	C.E_NO_ROOM:         "No room",
+	C.E_NOT_POSTED:      "Form has not been posted",
+	C.E_UNKNOWN_COMMAND: "Unknown command",
+	C.E_NO_MATCH:        "No match",
+	C.E_NOT_SELECTABLE:  "Not selectable",
+	C.E_NOT_CONNECTED:   "Field is not connected to a form",
+	C.E_REQUEST_DENIED:  "Request denied",
+	C.E_INVALID_FIELD:   "Invalid field",
+	C.E_CURRENT:         "Current",
+}
+
+func ncursesError(e error) error {
+	errno, ok := e.(syscall.Errno)
+	if int(errno) == C.OK {
+		e = nil
+	}
+	if ok {
+		errstr, ok := errList[C.int(errno)]
+		if ok {
+			return errors.New(errstr)
+		}
+	}
+	return e
 }
 
 type Menu struct {
@@ -268,7 +301,7 @@ func (m *Menu) SetSpacing(desc, row, col int) error {
 
 // SetWindow container for the menu
 func (m *Menu) SetWindow(w *Window) error {
-	err := C.set_menu_win(m.menu, w.win)
+	err := C.set_menu_win(m.menu, (*C.WINDOW)(w))
 	return ncursesError(syscall.Errno(err))
 }
 
