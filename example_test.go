5c11bd81a1c7cd49b960ef614c534eaea35d68ef
Rob Thornton <rthornton128@gmail.com>
2012-07-21 20:40:21 -0700

    Add another Init example

--- example_test.go
+++ example_test.go
@@ -1,9 +1,25 @@
 // ncurses_example.go
 package goncurses
 
-import "code.google.com/p/goncurses"
+import (
+	"code.google.com/p/goncurses"
+	"os"
+)
 
 func ExampleInit() {
 	goncurses.Init()
 	defer goncurses.End()
+}
+
+func ExampleInit_withPrint() {
+	// A full example can be found in examples/hello
+	stdscr, err := goncurses.Init()
+	defer goncurses.End()
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	stdscr.Print("Hello!")
+	stdscr.GetChar()
 }
