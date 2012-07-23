550061b1d358b28e90b4b49e8407ac8af4d7d7a2
Rob Thornton <rthornton128@gmail.com>
2012-07-22 17:22:55 -0700

    Move panel extension back to root and only build on posix systems

--- panel_posix.go
+++ panel_posix.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build linux bsd unix
+
 /* ncurses panel extension
 
 The following functions have not been implemented because there are far more
