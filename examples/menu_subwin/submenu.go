53d141f14c3bae2fed3b9ebdc7bbbc62917ce147
Rob Thornton <rthornton128@gmail.com>
2012-12-02 13:55:53 -0800

    Update submenu example

--- examples/menu_subwin/submenu.go
+++ examples/menu_subwin/submenu.go
@@ -3,69 +3,70 @@
 
 package main
 
-import . "goncurses.googlecode.com/hg/goncurses"
+import gc "code.google.com/p/goncurses"
 
 func main() {
-    stdscr, _ := Init();
-    defer End()
-    
-    StartColor()
-    Raw(true)
-    Echo(false)
-    Cursor(0)
-    stdscr.Keypad(true)
-    InitPair(1, C_RED, C_BLACK)
-    
-    // build the menu items
-    menu_items := []string{"Choice 1", "Choice 2", "Choice 3", "Choice 4", 
-        "Exit"}
-    items := make([]*MenuItem, len(menu_items))
-    for i, val := range menu_items {
-        items[i], _ = NewItem(val, "")
-        defer items[i].Free()
-    }
-    
-    // create the menu
-    menu, _ := NewMenu(items)
-    defer menu.Free()
-    
-    menuwin, _ := NewWindow(10, 40, 4, 14)
-    menuwin.Keypad(true)
-    
-    menu.Window(menuwin)
-    menu.SubWindow(menuwin.Derived(6, 38, 3, 1))
-    menu.Mark(" * ")
-    
-    // Print centered menu title
-    y, x := menuwin.Maxyx()
-    title := "My Menu"
-    menuwin.Box(0, 0)
-    menuwin.ColorOn(1)
-    menuwin.Print(1, (x/2)-(len(title)/2), title)
-    menuwin.ColorOff(1)
-    menuwin.AddChar(2, 0, ACS_LTEE)
-    menuwin.HLine(2, 1, ACS_HLINE, x-2)
-    menuwin.AddChar(2, x-1, ACS_RTEE)
-    
-    y, x = stdscr.Maxyx()
-    stdscr.Print(y-2, 1, "'q' to exit")
-    stdscr.Refresh()
-    
-    menu.Post()
-    defer menu.UnPost()
-    menuwin.Refresh()
-    
-    for {
-        Update()
-        ch := menuwin.GetChar()
-        
-        switch ch {
-        case 'q':
-            return
-        case KEY_DOWN:
-            menu.Driver(REQ_DOWN)
-        case KEY_UP:
-            menu.Driver(REQ_UP)
-        }
-    }
+	stdscr, _ := gc.Init()
+	defer gc.End()
+
+	gc.StartColor()
+	gc.Raw(true)
+	gc.Echo(false)
+	gc.Cursor(0)
+	stdscr.Keypad(true)
+	gc.InitPair(1, gc.C_RED, gc.C_BLACK)
+
+	// build the menu items
+	menu_items := []string{"Choice 1", "Choice 2", "Choice 3", "Choice 4",
+		"Exit"}
+	items := make([]*gc.MenuItem, len(menu_items))
+	for i, val := range menu_items {
+		items[i], _ = gc.NewItem(val, "")
+		defer items[i].Free()
+	}
+
+	// create the menu
+	menu, _ := gc.NewMenu(items)
+	defer menu.Free()
+
+	menuwin, _ := gc.NewWindow(10, 40, 4, 14)
+	menuwin.Keypad(true)
+
+	menu.SetWindow(&menuwin)
+	dwin := menuwin.Derived(6, 38, 3, 1)
+	menu.SubWindow(&dwin)
+	menu.Mark(" * ")
+
+	// Print centered menu title
+	y, x := menuwin.Maxyx()
+	title := "My Menu"
+	menuwin.Box(0, 0)
+	menuwin.ColorOn(1)
+	menuwin.Print(1, (x/2)-(len(title)/2), title)
+	menuwin.ColorOff(1)
+	menuwin.MoveAddChar(2, 0, gc.ACS_LTEE)
+	menuwin.HLine(2, 1, gc.ACS_HLINE, x-3)
+	menuwin.MoveAddChar(2, x-2, gc.ACS_RTEE)
+
+	y, x = stdscr.Maxyx()
+	stdscr.Print(y-2, 1, "'q' to exit")
+	stdscr.Refresh()
+
+	menu.Post()
+	defer menu.UnPost()
+	menuwin.Refresh()
+
+	for {
+		gc.Update()
+		ch := menuwin.GetChar()
+
+		switch ch {
+		case 'q':
+			return
+		case gc.KEY_DOWN:
+			menu.Driver(gc.REQ_DOWN)
+		case gc.KEY_UP:
+			menu.Driver(gc.REQ_UP)
+		}
+	}
 }
