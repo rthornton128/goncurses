b30ef97ac5f72f1936af947a1f8951b83097a35e
Rob Thornton <rthornton128@gmail.com>
2012-12-02 11:48:02 -0800

    Update panel2 example

--- examples/panel2/panel2.go
+++ examples/panel2/panel2.go
@@ -2,57 +2,57 @@
 
 package main
 
-import . "goncurses.googlecode.com/hg/goncurses"
+import gc "code.google.com/p/goncurses"
 
 func main() {
-    stdscr, _ := Init()
-    defer End()
+	stdscr, _ := gc.Init()
+	defer gc.End()
 
-    StartColor()
-    CBreak(true)
-    Echo(true)
-    stdscr.Keypad(true)
-    stdscr.Print("Hit 'tab' to cycle through windows, 'q' to quit")
-    
-    InitPair(1, C_RED, C_BLACK)
-    InitPair(2, C_GREEN, C_BLACK)
-    InitPair(3, C_BLUE, C_BLACK)
-    InitPair(4, C_CYAN, C_BLACK)
-    
-    var panels [3]*Panel
-    y, x := 4, 10
-    
-    for i := 0; i < 3; i++ {
-        h, w := 10, 40
-        title := "Window Number %d"
-        
-        window, _ := NewWindow(h, w , y+(i*4), x+(i*10))
-        window.Box(0, 0)
-        window.AddChar(2, 0, ACS_LTEE)
-        window.HLine(2, 1, ACS_HLINE, w - 2)
-        window.AddChar(2, w-1, ACS_RTEE)
-        window.ColorOn(byte(i+1))
-        window.Print(1, (w/2)-(len(title)/2), title, i+1)
-        window.ColorOff(byte(i+1))
-        panels[i] = NewPanel(window)
-        
-    }
+	gc.StartColor()
+	gc.CBreak(true)
+	gc.Echo(true)
+	stdscr.Keypad(true)
+	stdscr.Print("Hit 'tab' to cycle through windows, 'q' to quit")
 
-    active := 2
-    
-    for {
-        UpdatePanels()
-        Update()
-        
-        switch stdscr.GetChar() {
-        case 'q':
-            return
-        case KEY_TAB:
-            active += 1
-            if active > 2 {
-                active = 0
-            }
-            panels[active].Top()
-        }
-    }
+	gc.InitPair(1, gc.C_RED, gc.C_BLACK)
+	gc.InitPair(2, gc.C_GREEN, gc.C_BLACK)
+	gc.InitPair(3, gc.C_BLUE, gc.C_BLACK)
+	gc.InitPair(4, gc.C_CYAN, gc.C_BLACK)
+
+	var panels [3]*gc.Panel
+	y, x := 4, 10
+
+	for i := 0; i < 3; i++ {
+		h, w := 10, 40
+		title := "Window Number %d"
+
+		window, _ := gc.NewWindow(h, w, y+(i*4), x+(i*10))
+		window.Box(0, 0)
+		window.MoveAddChar(2, 0, gc.ACS_LTEE)
+		window.HLine(2, 1, gc.ACS_HLINE, w-2)
+		window.MoveAddChar(2, w-1, gc.ACS_RTEE)
+		window.ColorOn(byte(i + 1))
+		window.Print(1, (w/2)-(len(title)/2), title, i+1)
+		window.ColorOff(byte(i + 1))
+		panels[i] = gc.NewPanel(&window)
+
+	}
+
+	active := 2
+
+	for {
+		gc.UpdatePanels()
+		gc.Update()
+
+		switch stdscr.GetChar() {
+		case 'q':
+			return
+		case gc.KEY_TAB:
+			active += 1
+			if active > 2 {
+				active = 0
+			}
+			panels[active].Top()
+		}
+	}
 }
