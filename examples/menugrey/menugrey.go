9457da3368379194e1d1f3919967a57ae67b031a
Rob Thornton <rthornton128@gmail.com>
2012-12-02 14:26:13 -0800

    Update menugrey example

--- examples/menugrey/menugrey.go
+++ examples/menugrey/menugrey.go
@@ -3,69 +3,70 @@
 
 package main
 
-import . "goncurses.googlecode.com/hg/goncurses"
+import gc "code.google.com/p/goncurses"
 
 func main() {
-    stdscr, _ := Init();
-    defer End()
-    
-    StartColor()
-    Raw(true)
-    Echo(false)
-    Cursor(0)
-    stdscr.Keypad(true)
-    InitPair(1, C_RED, C_BLACK)
-    InitPair(2, C_GREEN, C_BLACK)
-    InitPair(3, C_MAGENTA, C_BLACK)
-    
-    // build the menu items
-    menu_items := []string{
-        "Choice 1", 
-        "Choice 2", 
-        "Choice 3", 
-        "Choice 4", 
-        "Choice 5",
-        "Exit"}
-    items := make([]*MenuItem, len(menu_items))
-    for i, val := range menu_items {
-        items[i], _ = NewItem(val, "")
-        defer items[i].Free()
-        
-        if i == 2 || i == 4 {
-            items[i].Selectable(false)
-        }
-    }
-    
-    // create the menu
-    menu, _ := NewMenu(items)
-    defer menu.Free()
-    
-    y, _ := stdscr.Maxyx()
-    stdscr.Print(y-3, "Use up/down arrows to move; 'q' to exit")
-    stdscr.Refresh()
+	stdscr, _ := gc.Init()
+	defer gc.End()
 
-    menu.Foreground(ColorPair(1)|A_REVERSE)
-    menu.Background(ColorPair(2)|A_BOLD)
-    menu.Grey(ColorPair(3)|A_BOLD)
-    
-    menu.Post()
-    defer menu.UnPost()
-    
-    for {
-        Update()
-        ch := stdscr.GetChar()
-        switch ch {
-        case ' ':
-            menu.Driver(REQ_TOGGLE)
-        case 'q':
-            return
-        case KEY_RETURN:
-            stdscr.Move(20, 0)
-            stdscr.ClearToEOL()
-            stdscr.Print(20, 0, "Item selected is: %s", menu.Current(nil).Name())
-            menu.PositionCursor()
-        default:
-            menu.Driver(DriverActions[ch])
-        }
-    }
+	gc.StartColor()
+	gc.Raw(true)
+	gc.Echo(false)
+	gc.Cursor(0)
+	stdscr.Keypad(true)
+	gc.InitPair(1, gc.C_RED, gc.C_BLACK)
+	gc.InitPair(2, gc.C_GREEN, gc.C_BLACK)
+	gc.InitPair(3, gc.C_MAGENTA, gc.C_BLACK)
+
+	// build the menu items
+	menu_items := []string{
+		"Choice 1",
+		"Choice 2",
+		"Choice 3",
+		"Choice 4",
+		"Choice 5",
+		"Exit"}
+	items := make([]*gc.MenuItem, len(menu_items))
+	for i, val := range menu_items {
+		items[i], _ = gc.NewItem(val, "")
+		defer items[i].Free()
+
+		if i == 2 || i == 4 {
+			items[i].Selectable(false)
+		}
+	}
+
+	// create the menu
+	menu, _ := gc.NewMenu(items)
+	defer menu.Free()
+
+	y, _ := stdscr.Maxyx()
+	stdscr.Print(y-3, "Use up/down arrows to move; 'q' to exit")
+	stdscr.Refresh()
+
+	menu.SetForeground(gc.ColorPair(1) | gc.A_REVERSE)
+	menu.SetBackground(gc.ColorPair(2) | gc.A_BOLD)
+	menu.Grey(gc.ColorPair(3) | gc.A_BOLD)
+
+	menu.Post()
+	defer menu.UnPost()
+
+	for {
+		gc.Update()
+		ch := stdscr.GetChar()
+		switch ch {
+		case ' ':
+			menu.Driver(gc.REQ_TOGGLE)
+		case 'q':
+			return
+		case gc.KEY_RETURN:
+			stdscr.Move(20, 0)
+			stdscr.ClearToEOL()
+			stdscr.Print(20, 0, "Item selected is: %s",
+				menu.Current(nil).Name())
+			menu.PositionCursor()
+		default:
+			menu.Driver(gc.DriverActions[ch])
+		}
+	}
 }
