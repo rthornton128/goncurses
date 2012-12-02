38c25d2325c439acb7543cf41e179f500b4b84d5
Rob Thornton <rthornton128@gmail.com>
2012-12-02 13:13:08 -0800

    Update menutoggle example

--- examples/menutoggle/menutoggle.go
+++ examples/menutoggle/menutoggle.go
@@ -3,70 +3,70 @@
 
 package main
 
-import . "goncurses.googlecode.com/hg/goncurses"
+import gc "code.google.com/p/goncurses"
 
 func main() {
-    stdscr, _ := Init();
-    defer End()
-    
-    StartColor()
-    Raw(true)
-    Echo(false)
-    Cursor(0)
-    stdscr.Keypad(true)
-    
-    // build the menu items
-    menu_items := []string{
-        "Choice 1", 
-        "Choice 2", 
-        "Choice 3", 
-        "Choice 4", 
-        "Choice 5", 
-        "Choice 6", 
-        "Choice 7",
-        "Exit"}
-    items := make([]*MenuItem, len(menu_items))
-    for i, val := range menu_items {
-        items[i], _ = NewItem(val, "")
-        defer items[i].Free()
-    }
-    
-    // create the menu
-    menu, _ := NewMenu(items)
-    defer menu.Free()
+	stdscr, _ := gc.Init()
+	defer gc.End()
 
-    menu.Option(O_ONEVALUE, false)
-    
-    y, _ := stdscr.Maxyx()
-    stdscr.Print(y-3, "Use up/down arrows to move, spacebar to toggle and " +
-        "enter to print. 'q' to exit")
-    stdscr.Refresh()
-    
-    menu.Post()
-    defer menu.UnPost()
-    
-    for {
-        Update()
-        ch := stdscr.GetChar()
+	gc.StartColor()
+	gc.Raw(true)
+	gc.Echo(false)
+	gc.Cursor(0)
+	stdscr.Keypad(true)
 
-        switch ch {
-        case 'q':
-            return
-        case ' ':
-            menu.Driver(REQ_TOGGLE)
-        case KEY_RETURN:
-            var list string
-            for _, item := range menu.Items(nil) {
-                if item.Value() {
-                    list += "\"" + item.Name() + "\" "
-                }
-            }
-            stdscr.Move(20, 0)
-            stdscr.ClearToEOL()
-            stdscr.Print(20, list)
-            stdscr.Refresh()
-        default:
-            menu.Driver(DriverActions[ch])
-        }
-    }
+	// build the menu items
+	menu_items := []string{
+		"Choice 1",
+		"Choice 2",
+		"Choice 3",
+		"Choice 4",
+		"Choice 5",
+		"Choice 6",
+		"Choice 7",
+		"Exit"}
+	items := make([]*gc.MenuItem, len(menu_items))
+	for i, val := range menu_items {
+		items[i], _ = gc.NewItem(val, "")
+		defer items[i].Free()
+	}
+
+	// create the menu
+	menu, _ := gc.NewMenu(items)
+	defer menu.Free()
+
+	menu.Option(gc.O_ONEVALUE, false)
+
+	y, _ := stdscr.Maxyx()
+	stdscr.Print(y-3, "Use up/down arrows to move, spacebar to toggle and "+
+		"enter to print. 'q' to exit")
+	stdscr.Refresh()
+
+	menu.Post()
+	defer menu.UnPost()
+
+	for {
+		gc.Update()
+		ch := stdscr.GetChar()
+
+		switch ch {
+		case 'q':
+			return
+		case ' ':
+			menu.Driver(gc.REQ_TOGGLE)
+		case gc.KEY_RETURN:
+			var list string
+			for _, item := range menu.Items() {
+				if item.Value() {
+					list += "\"" + item.Name() + "\" "
+				}
+			}
+			stdscr.Move(20, 0)
+			stdscr.ClearToEOL()
+			stdscr.Print(20, list)
+			stdscr.Refresh()
+		default:
+			menu.Driver(gc.DriverActions[ch])
+		}
+	}
 }
