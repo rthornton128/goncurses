e6e4b9764cafa512bcae30edb9dc04bda96ae91c
Rob Thornton <rthornton128@gmail.com>
2012-12-02 10:33:13 -0800

    Update print example and format with go fmt

--- examples/print/print.go
+++ examples/print/print.go
@@ -1,23 +1,22 @@
-/* This example mirrors the second example in the TLDP ncurses howto,
-    demonstrating some of the initilization options for ncurses;
-    In gnome, the F1 key launches help, so F2 is tested for instead */
+/* This example demonstrates the use of the print function. */
 
 package main
 
-import . "goncurses.googlecode.com/hg/goncurses"
+import gc "code.google.com/p/goncurses"
 
 func main() {
-    stdscr, _ := Init();
-    defer End()
-    
-    row, col := stdscr.Maxyx()
-    msg := "Just a string "
-    stdscr.Print(row/2, (col-len(msg))/2, msg)
-    
-    stdscr.Print(row-2, 0, "This screen has %d rows and %d columns. ",
-        row, col)
-    stdscr.Print("Try resizing your window and then run this program again.")
+	stdscr, _ := gc.Init()
+	defer gc.End()
 
-    stdscr.Refresh()
-    stdscr.GetChar()
+	row, col := stdscr.Maxyx()
+	msg := "Just a string "
+	stdscr.Print(row/2, (col-len(msg))/2, msg)
+
+	stdscr.Print(row-3, 0, "This screen has %d rows and %d columns. ",
+		row, col)
+	stdscr.Print(row-2, 0, "Try resizing your window and then run this "+
+		"program again.")
+
+	stdscr.Refresh()
+	stdscr.GetChar()
 }
