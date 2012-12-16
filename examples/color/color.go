78079525099ed1be0671765239535e941c7641c2
Rob Thornton <rthornton128@gmail.com>
2012-12-15 19:53:39 -0800

    go fmt color example

--- examples/color/color.go
+++ examples/color/color.go
@@ -6,27 +6,27 @@ package main
 import . "code.google.com/p/goncurses"
 
 func main() {
-    stdscr, _ := Init()
-    defer End()
-    StartColor()
-    
-    Raw(true)
-    Echo(true)
-    InitPair(1, C_BLUE, C_WHITE)
-    InitPair(2, C_BLACK, C_CYAN)
-    
-    // An example of trying to set an invalid color pair
-    err := InitPair(255, C_BLACK, C_CYAN)
-    stdscr.Print("An intentional error: %s", err.Error())
-    
-    stdscr.Keypad(true)
-    stdscr.Print(12, 30, "Hello, World!!!")
-    stdscr.Refresh()
-    stdscr.GetChar()
-    stdscr.Background(ColorPair(2))
-    stdscr.ColorOn(1)
-    stdscr.Print(13, 30, "Hello, World in Color!!!")
-    stdscr.ColorOff(1)
-    stdscr.Refresh()
-    stdscr.GetChar()
+	stdscr, _ := Init()
+	defer End()
+	StartColor()
+
+	Raw(true)
+	Echo(true)
+	InitPair(1, C_BLUE, C_WHITE)
+	InitPair(2, C_BLACK, C_CYAN)
+
+	// An example of trying to set an invalid color pair
+	err := InitPair(255, C_BLACK, C_CYAN)
+	stdscr.Print("An intentional error: %s", err.Error())
+
+	stdscr.Keypad(true)
+	stdscr.Print(12, 30, "Hello, World!!!")
+	stdscr.Refresh()
+	stdscr.GetChar()
+	stdscr.Background(ColorPair(2))
+	stdscr.ColorOn(1)
+	stdscr.Print(13, 30, "Hello, World in Color!!!")
+	stdscr.ColorOff(1)
+	stdscr.Refresh()
+	stdscr.GetChar()
 }
