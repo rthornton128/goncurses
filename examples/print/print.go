2ddced8558abeb8a85ced4ae14a8231c42dad3f4
Rob Thornton <rthornton128@gmail.com>
2012-12-15 20:44:54 -0800

    Update print example for MovePrint

--- examples/print/print.go
+++ examples/print/print.go
@@ -10,11 +10,11 @@ func main() {
 
 	row, col := stdscr.Maxyx()
 	msg := "Just a string "
-	stdscr.Print(row/2, (col-len(msg))/2, msg)
+	stdscr.MovePrint(row/2, (col-len(msg))/2, msg)
 
-	stdscr.Print(row-3, 0, "This screen has %d rows and %d columns. ",
+	stdscr.MovePrint(row-3, 0, "This screen has %d rows and %d columns. ",
 		row, col)
-	stdscr.Print(row-2, 0, "Try resizing your window and then run this "+
+	stdscr.MovePrint(row-2, 0, "Try resizing your window and then run this "+
 		"program again.")
 
 	stdscr.Refresh()
