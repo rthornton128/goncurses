bb07063d1a8ba01c8938f3c897a1927401cd5982
Rob Thornton <rthornton128@gmail.com>
2012-12-15 21:56:50 -0800

    Add an example for creating and using a pad

---
+++ examples/pad/pad.go
@@ -0,0 +1,20 @@
+// A basic example of how to create and display a pad
+package main
+
+import gc "code.google.com/p/goncurses"
+
+func main() {
+	gc.Init()
+	defer gc.End()
+
+	// create a new pad of 30 rows and 100 columns and fill it
+	pad := gc.NewPad(30, 100)
+	for x := 1; x < 30; x++ {
+		pad.Window().MovePrint(x, x, "This is a pad.")
+	}
+	// show a 10x15 portion of the pad located at the coordinates
+	// 5, 10 on the stdscr. The portion of the pad shown is a box
+	// starting at 0, 5 to 10, 20
+	pad.Refresh(0, 5, 5, 10, 15, 25)
+	pad.Window().GetChar()
+}
