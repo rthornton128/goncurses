12962145a9cb1d9a8e8818534fcb9a677d93259d
Rob Thornton <rthornton128@gmail.com>
2013-02-05 19:31:48 -0800

    Add license header to goncurses.h

--- goncurses.h
+++ goncurses.h
@@ -1,3 +1,7 @@
+// Copyright 2011 Rob Thornton. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 chtype ncurses_getbkgd(WINDOW *win);
 void ncurses_getmaxyx(WINDOW *win, int *y, int *x);
 void ncurses_getyx(WINDOW *win, int *y, int *x);
