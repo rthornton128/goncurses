efbdb67dd6b6b3c666700e1f679ab112a6fbe8f1
Rob Thornton <rthornton128@gmail.com>
2013-05-21 19:35:51 -0700

    Remove duplicate example and fix example_test.go

--- example_test.go
+++ example_test.go
@@ -1,23 +1,11 @@
 // ncurses_example.go
-package goncurses
+package goncurses_test
 
 import (
 	"code.google.com/p/goncurses"
 	"fmt"
 	"os"
 )
-
-func ExampleEnd() {
-	// You should always test to make sure ncurses has initialized properly.
-	// In order for your error messages to be visible on the terminal you will
-	// need to either log error messages or output them to to stderr.
-	stdscr, err := goncurses.Init()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	defer goncurses.End()
-}
 
 func ExampleInit() {
 	// You should always test to make sure ncurses has initialized properly.
@@ -29,4 +17,6 @@ func ExampleInit() {
 		os.Exit(1)
 	}
 	defer goncurses.End()
+	stdscr.Print("Press enter to continue...")
+	stdscr.Refresh()
 }
