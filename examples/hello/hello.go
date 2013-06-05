229abb15f8e94b5ded37edba03617d820e5eb1b8
Rob Thornton <rthornton128@gmail.com>
2013-06-04 19:14:18 -0700

    Improve hello world example

--- examples/hello/hello.go
+++ examples/hello/hello.go
@@ -2,13 +2,22 @@
 
 package main
 
-import "code.google.com/p/goncurses"
+import (
+	"code.google.com/p/goncurses"
+	"log"
+)
 
 func main() {
-	stdscr, _ := goncurses.Init()
+	// Initialize goncurses. It's essential End() is called to ensure the
+	// terminal isn't altered after the program ends
+	stdscr, err := goncurses.Init()
+	if err != nil {
+		log.Fatal("init", err)
+	}
 	defer goncurses.End()
 
 	stdscr.Print("Hello, World!!!")
+	stdscr.MovePrint(3, 0, "Press any key to continue")
 	stdscr.Refresh()
 	stdscr.GetChar()
 }
