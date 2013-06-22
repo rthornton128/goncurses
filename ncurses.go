080ee1de499f5c359b0c7a292843d5ffcf498dba
Rob Thornton <rthornton128@gmail.com>
2013-06-22 12:46:49 -0700

    Add warning about concurrency

--- ncurses.go
+++ ncurses.go
@@ -19,6 +19,19 @@
 // fail to do so, the terminal will not perform properly and will either
 // need to be reset or restarted completely.
 //
+// CAUTION: Calls to ncurses functions are normally not atomic nor reentrant
+// and therefore extreme care should be taken to ensure ncurses functions
+// are not called concurrently. Specifically, never write data to the same
+// window concurrently nor accept input and send output to the same window as
+// both alter the underlying C data structures in a non safe manner.
+//
+// Ideally, you should structure your program to ensure all ncurses related
+// calls happen in a single goroutine. This is probably most easily achieved
+// via channels and Go's built-in select. Alternatively, or additionally, you
+// can use a mutex to protect any calls in multiple goroutines from happening
+// concurrently. Failure to do so will result in unpredictable and
+// undefined behaviour in your program.
+//
 // The examples directory contains demontrations of many of the capabilities
 // goncurses can provide.
 package goncurses
