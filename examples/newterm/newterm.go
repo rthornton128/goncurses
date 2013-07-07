a099199c45ab06f857d5efea2009e42a8bea5f8c
Rob Thornton <rthornton128@gmail.com>
2013-07-06 20:05:13 -0700

    Implement screen related functions for multiple terminals along with an example

---
+++ examples/newterm/newterm.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	gc "code.google.com/p/goncurses"
+	"log"
+	"os"
+)
+
+func main() {
+	var term1, term2 *gc.Screen
+	var err error
+
+	// You can, for example, use the current psuedo-terminal to read/write to
+	var pts *os.File
+	pts, err = os.OpenFile("/dev/pts/0", os.O_RDWR, os.FileMode(666))
+	if err != nil {
+		log.Fatal(nil)
+	}
+	defer pts.Close()
+
+	// Create a new terminal using the default $TERM type and the psuedo-terminal
+	term1, err = gc.NewTerm("", pts, pts)
+	if err != nil {
+		log.Fatal("newterm:", err)
+	}
+	// Remember that defer is LIFO order and End must be called prior to Delete
+	defer term1.Delete()
+	defer term1.End()
+
+	// Create a second terminal as if we had two to interact with
+	term2, err = gc.NewTerm("", os.Stdout, os.Stdin)
+	if err != nil {
+		log.Fatal("newterm:", err)
+	}
+	// Remember that defer is LIFO order and End must be called prior to Delete
+	// It also matters than End is called on term2 prior to term1
+	defer term2.Delete()
+	defer term2.End()
+
+	// Set the active terminal to term1
+	term1.Set()
+
+	// Get the Standard Screen Window and write to the active terminal
+	mw := gc.StdScr() // misleading...use a StdScr() method instead?
+	mw.MovePrint(0,0, "Term 1 works! Press any key to exit...")
+	mw.Refresh()
+	mw.GetChar()
+
+	// activate term2 and send data to it
+	term2.Set()
+	mw.MovePrint(0,0, "Term 2 works! Press any key to exit...")
+	mw.Refresh()
+	mw.GetChar()
+}
---
+++ screen.go
@@ -0,0 +1,67 @@
+package goncurses
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <ncurses.h>
+import "C"
+
+import (
+	"errors"
+	"os"
+	"unsafe"
+)
+
+type Screen struct { scrPtr *C.SCREEN }
+
+// NewTerm returns a new Screen, representing a physical terminal. If using
+// this function to generate a new Screen you should not call Init().
+// Unlike Init(), NewTerm does not call Refresh() to clear the screen so this
+// will need to be done manually. When finished with a terminal, you must
+// call End() in reverse order that each terminal was created in. After you
+// are finished with the screen you must call Delete to free the memory
+// allocated to it. This function is usually only useful for programs using
+// multiple terminals or test for terminal capabilites. The argument termType
+// is the type of terminal to be used ($TERM is used if value is "" which also
+// has the same effect of using os.Getenv("TERM"))
+func NewTerm(termType string, out, in *os.File) (*Screen, error) {
+	var tt, wr, rd *C.char
+	if termType == "" {
+		tt, wr, rd = (*C.char)(nil), C.CString("w"), C.CString("r")
+	} else {
+		tt, wr, rd = C.CString(termType), C.CString("w"), C.CString("r")
+		defer C.free(unsafe.Pointer(tt))
+	}
+	defer C.free(unsafe.Pointer(wr))
+	defer C.free(unsafe.Pointer(rd))
+
+	cout, cin := C.fdopen(C.int(out.Fd()), wr), C.fdopen(C.int(in.Fd()), rd)
+	screen := C.newterm(tt, cout, cin)
+	if screen == nil {
+		return nil, errors.New("Failed to create new screen")
+	}
+	return &Screen{screen}, nil
+}
+
+// Set the screen to be the current, active screen
+func (s *Screen) Set() (*Screen, error) {
+	screen := C.set_term(s.scrPtr)
+	if screen == nil {
+		return nil, errors.New("Failed to set screen")
+	}
+	return &Screen{screen}, nil
+}
+
+// Delete frees memory allocated to the screen. This function 
+func (s *Screen) Delete() {
+	C.delscreen(s.scrPtr)
+}
+
+// End is just a wrapper for the global End function. This helper function
+// has been provided to help ensure that new terminals are closed in the
+// proper, reverse order they were created. It makes the terminal active via
+// set then called End so it is closed properly. You must make sure that
+// Delete is called once done with the screen/terminal.
+func (s *Screen) End() {
+	s.Set()
+	End()
+}
