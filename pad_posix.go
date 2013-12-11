361919303e13fe34c94854e5a1ea924b3cdf94c0
Rob Thornton <rthornton128@gmail.com>
2013-12-11 12:08:28 -0800

    Re-enable Echo on pads for ncurses users and fix bug in GetChar when using timeouts

---
+++ pad_posix.go
@@ -0,0 +1,18 @@
+// goncurses - ncurses library for Go.
+// Copyright 2011 Rob Thornton. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build !windows
+
+package goncurses
+
+// #include <curses.h>
+import "C"
+
+// Echo prints a single character to the pad immediately. This has the
+// same effect of calling AddChar() + Refresh() but has a significant
+// speed advantage
+func (p Pad) Echo(ch int) {
+	C.pechochar(p.win, C.chtype(ch))
+}
