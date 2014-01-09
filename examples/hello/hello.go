cb556e2ad60002d4afb761e96afdfc1e81658478
Rob Thornton <rthornton128@gmail.com>
2014-01-09 09:03:04 -0800

    Update hello example with better documentation and minor tweaks

--- examples/hello/hello.go
+++ examples/hello/hello.go
@@ -3,7 +3,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-/* This simple example mirrors the "hello world" TLDP ncurses howto */
+/* The classic "Hello, World!" program in Goncurses! */
 
 package main
 
@@ -21,8 +21,19 @@ func main() {
 	}
 	defer goncurses.End()
 
-	stdscr.Print("Hello, World!!!")
+	// (Go)ncurses draws by cursor position and uses reverse cartisian
+	// coordinate system (y,x). Initially, the cursor is positioned at the
+	// coordinates 0,0 so the first call to Print will output the text at the
+	// top, left position of the screen since stdscr is a window which
+	// represents the terminal's screen size.
+	stdscr.Print("Hello, World!")
 	stdscr.MovePrint(3, 0, "Press any key to continue")
+
+	// Refresh() flushes output to the screen. Internally, it is the same as
+	// calling NoutRefresh() on the window followed by a call to Update()
 	stdscr.Refresh()
+
+	// GetChar will block execution until an input event, like typing a
+	// character on your keyboard, is received
 	stdscr.GetChar()
 }
