ceb562ffc5e4db0816c0e218f460d21f71bc917d
Mike Brown <github@torvosoft.com>
2026-01-19 13:45:09 -0500

    Add {Def,Reset}{Prog,Shell}Mode
    This commit adds go bindings for def_prog_mode(), reset_prog_mode(),
    def_shell_mode(), and reset_shell_mode(). see
    https://pubs.opengroup.org/onlinepubs/7908799/xcurses/def_prog_mode.html
    for further detail.

--- ncurses.go
+++ ncurses.go
@@ -318,3 +318,25 @@ func SetTabSize(tabSize int) {
 func TabSize() int {
 	return int(C.TABSIZE)
 }
+
+// DefProgMode saves the current terminal modes as the "program" (in Curses)
+// state for use by reset_prog_mode().
+func DefProgMode() int {
+	return int(C.def_prog_mode())
+}
+
+// ResetProgMode restores the terminal to the "program" (in Curses) state.
+func ResetProgMode() int {
+	return int(C.reset_prog_mode())
+}
+
+// DefShellMode saves the current terminal modes as the "shell" (not in Curses)
+// state for use by reset_prog_mode().
+func DefShellMode() int {
+	return int(C.def_shell_mode())
+}
+
+// ResetShellMode restores the terminal to the "shell" (not in Curses) state.
+func ResetShellMode() int {
+	return int(C.reset_shell_mode())
+}
